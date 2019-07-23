93445428efb0b98297f7343b5fda06b592016e25
ksmo <715705307@qq.com>
2019-07-23 20:04:45 +0800

    update dscn

--- deepctr/models/dscn.py
+++ deepctr/models/dscn.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from ..inputs import input_from_feature_columns,build_input_features,combined_dnn_input
 from ..layers.core import PredictionLayer, DNN
 from ..layers.interaction import CrossNet
-from tensorflow.python.keras.layers import Embedding, Input, Flatten
+from tensorflow.python.keras.layers import Embedding, Input, Flatten, Lambda, LSTM
 from base_model import *
 
 
@@ -29,30 +29,30 @@ def DSCN(embedding_size='auto', cross_num=2, dnn_hidden_units=(1000, 512,), l2_r
     dense_layers = tf.keras.Sequential([tf.keras.layers.Dense(dim, activation='relu') for dim in dnn_hidden_units])
 
     with tf.name_scope('embedding'):
-        factor_embedding = init_embedding('factor_embedding', factor_num, factor_embedding_dim)
-        input_factor_embedding = tf.nn.embedding_lookup(factor_embedding, input_factor)
+        # factor_embedding = init_embedding('factor_embedding', factor_num, factor_embedding_dim)
+        input_factor_embedding = Embedding(factor_num, factor_embedding_dim, mask_zero=True)(input_factor)
+        # input_factor_embedding = tf.nn.embedding_lookup(factor_embedding, input_factor)
         # input_factor_feature = tf.reduce_mean(input_factor_embedding, axis=1)
         input_factor_feature = get_MIL_att(input_factor_embedding)
 
-	action_embedding = init_embedding('action_embedding', action_num, action_embedding_dim)
-	service_embedding = init_embedding('service_embedding', service_num, service_embedding_dim)
-	input_action_embedding = tf.nn.embedding_lookup(action_embedding, input_action)
-	input_service_embedding = tf.nn.embedding_lookup(service_embedding, input_service)
-	input_action_feature = get_lstm_output('action', input_action, input_action_embedding, action_embedding_dim)
-	input_service_feature = get_lstm_output('service', input_service, input_service_embedding, service_embedding_dim)
+    input_action_embedding = Embedding(action_num, action_embedding_dim, mask_zero=True)(input_action)
+    input_service_embedding = Embedding(service_num, service_embedding_dim, mask_zero=True)(input_service)
+    input_action_feature = LSTM(action_embedding_dim)(input_action_embedding)
+    input_service_feature = LSTM(service_embedding_dim)(input_service_embedding)
+	# input_action_feature = get_lstm_output('action', input_action, input_action_embedding, action_embedding_dim)
+	# input_service_feature = get_lstm_output('service', input_service, input_service_embedding, service_embedding_dim)
 
-    input_layer = input_factor_feature
+    input_layer = tf.keras.layers.concatenate([input_factor_feature, input_action_feature, input_service_feature], axis=1)
 
-    input_layer = tf.concat([input_layer, input_action_feature, input_service_feature], axis=1)
-
-    cross_output = get_cross_output(input_layer)
+    # cross_output = Lamda(get_cross_output)(input_layer)
+    cross_output = CrossNet(cross_num, l2_reg=l2_reg_cross)(input_layer)
 
     dnn_output = dense_layers(input_layer)
-    concat_output = tf.concat([cross_output, dnn_output, input_action_feature], axis=1)
+    concat_output = tf.keras.layers.concatenate([cross_output, dnn_output, input_action_feature], axis=1)
 
-    concat_output = get_dropout(concat_output)
+    concat_output = tf.keras.layers.Dropout(concat_output)
 
-    logits_stu = tf.layers.dense(inputs=concat_output, units=num_class, activation=None, name='latent')
+    logits_stu = Dense(num_class)(concat_output)
     pred_stu = PredictionLayer(num_class)(logits_stu)
 
     predict_result = pred_stu
